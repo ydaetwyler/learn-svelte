ca99116f77461fc9ba4da07efa34ef896ef2799e
ydaetwyler <ydaetwyler@previon.ch>
2023-05-20 00:45:30 +0200

    Nested & Log in Button Tutorial

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,15 @@
 <script>
+  // Nested import
+  import Nested from './Nested.svelte'
+
+  // Br import
+  import Br from './Br.svelte'
+
+  // Button
+  let user = { loggedIn: false }
+
+  const toggle = () => user.loggedIn = !user.loggedIn
+
   // cick count
   let count = 0
   $: doubled = count * 2
@@ -25,9 +36,32 @@
 
 <p>{count} doubled ist {doubled}</p>
 
+<Br />
+
 <!-- numbers -->
 <p>{numbers.join(' + ')} = {sum}</p>
 
 <button on:click={addNumber}>
   Add a number
 </button>
+
+<Br />
+
+<!-- Nested -->
+<Nested answer={43}/>
+<Nested/>
+
+<Br />
+
+<!-- Button -->
+{#if user.loggedIn}
+  <button on:click={toggle}>
+    Log out
+  </button>
+{/if}
+
+{#if !user.loggedIn}
+  <button on:click={toggle}>
+    Log in
+  </button>
+{/if}
---
+++ src/Nested.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let answer = 42
+</script>
+
+<p>The answer is {answer}</p>
